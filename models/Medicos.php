844961db2669b2854a3172e204c7a4a1a0292a7a
NerdThalesPalmas2025 <thalesmarques2025@gmail.com>
2025-06-30 19:49:28 -0300

    refactor: Renomeia modelos de singular para plural e ajusta Usuario

--- models/Medicos.php
+++ models/Medicos.php
@@ -1,7 +1,7 @@
 <?php
 require_once __DIR__ . '/../Database.php';
 
-class Medico {
+class Medicos {
     private $conn;
 
     public function __construct() {
--- models/Usuarios.php
+++ models/Usuarios.php
@@ -1,7 +1,7 @@
 <?php
 require_once __DIR__ . '/../Database.php';
 
-class Paciente {
+class Usuarios {
     private $conn;
 
     public function __construct() {
